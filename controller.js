997122b19e4fbf8a83021267b9417893d1dade83
kilowatt11 <kilowatt11@cableone.net>
2016-04-18 10:51:41 -0600

    Initial Commit

---
+++ controller.js
@@ -0,0 +1,162 @@
+app.controller('bubController', function ($scope) {
+    $scope.counter = 0;
+    $scope.meettxt = "waiting";
+    $scope.bumpUp = function () {
+        if($scope.counter < 5){
+        $scope.counter++
+        };
+        switch ($scope.counter) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                $scope.myStyle = {
+                    "width": "65%",
+                    "height": "65%"
+                };
+                $scope.upStyle = {
+                    "margin-left": "85%",
+                    "bottom": "85px"
+                };
+                $scope.downStyle = {
+                    "margin-left": "0%",
+                    "bottom": "85px"
+                }
+                $scope.popStyle = {
+                    "top": "170px",
+                    "margin-left": "45%"
+                }
+                break;
+
+            case 4:
+                $scope.myStyle = {
+                    "width": "75%",
+                    "height": "75%"
+                };
+                $scope.upStyle = {
+                    "margin-left": "88%",
+                    "bottom": "95px"
+                };
+                $scope.downStyle = {
+                    "margin-left": "0%",
+                    "bottom": "95px"
+                }
+                $scope.popStyle = {
+                    "top": "200px",
+                    "margin-left": "45%"
+                }
+                break;
+            case 5:
+                alert("Lets Meet!!");
+                break;
+            default:
+                $scope.myStyle = {
+                    "width": "45%",
+                    "height": "45%"
+                };
+                $scope.upStyle = {
+                    "margin-left": "80%",
+                    "bottom": "60px"
+                };
+                $scope.downStyle = {
+                    "margin-left": "0%",
+                    "bottom": "60px"
+                }
+                $scope.popStyle = {
+                    "top": "105px",
+                    "margin-left": "36%"
+                }
+                break;
+
+        }
+        //     if ($scope.counter === 1 || 2) {
+        //         return;
+
+        //     } else if ($scope.counter === 3) {
+        //         $scope.myStyle = {
+        //             "width": "65%",
+        //             "height": "65%"
+        //         }
+        //         $scope.counter++
+
+        //     } else if ($scope.counter === 4) {
+        //         $scope.myStyle = {
+        //             "width": "65%",
+        //             "height": "65%"
+        //         }
+        //         $scope.counter++
+        //     }
+    };
+    $scope.bumpDown = function () {
+        if($scope.counter > 0){
+        $scope.counter--
+        };
+        switch ($scope.counter) {
+            case 1:          
+            case 2:
+            default:
+            $scope.myStyle = {
+                    "width": "45%",
+                    "height": "45%"
+                };
+                $scope.upStyle = {
+                    "margin-left": "80%",
+                    "bottom": "60px"
+                };
+                $scope.downStyle = {
+                    "margin-left": "0%",
+                    "bottom": "60px"
+                }
+                $scope.popStyle = {
+                    "top": "105px",
+                    "margin-left": "36%"
+                }
+                break;
+  
+            case 3:
+                $scope.myStyle = {
+                    "width": "65%",
+                    "height": "65%"
+                };
+                $scope.upStyle = {
+                    "margin-left": "85%",
+                    "bottom": "85px"
+                };
+                $scope.downStyle = {
+                    "margin-left": "0%",
+                    "bottom": "85px"
+                }
+                $scope.popStyle = {
+                    "top": "170px",
+                    "margin-left": "45%"
+                }
+                break;
+
+            case 4:
+                $scope.myStyle = {
+                    "width": "75%",
+                    "height": "75%"
+                };
+                $scope.upStyle = {
+                    "margin-left": "88%",
+                    "bottom": "95px"
+                };
+                $scope.downStyle = {
+                    "margin-left": "0%",
+                    "bottom": "95px"
+                }
+                $scope.popStyle = {
+                    "top": "200px",
+                    "margin-left": "45%"
+                }
+                break;
+            case 5:
+                alert("Lets Meet!!");
+                break;
+         
+
+        }
+    };
+
+})
